c99d3d60e2a99539d2c060253b0abc6964b88ace
KobeMillward <jaikobework@gmail.com>
2023-09-15 06:17:13 +1000

    Added initial GUI

--- main.py
+++ main.py
@@ -2,28 +2,60 @@ import csv
 import folium
 import io
 import sys
-from PyQt5 import QtWidgets, QtWebEngineWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QPushButton
+from PyQt5 import QtWebEngineWidgets
 
-def update_map(web):
-    data_bytes = io.BytesIO()
-    new_map = folium.Map(location=[-25.290053, 152.864781])
-    new_map.save(data_bytes, close_file=False)
-    w.setHtml(data_bytes.getvalue().decode())
-    return data_bytes
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+def get_data():
+    pass
+
+
+def create_gui():
     m = folium.Map(location=[-27.470457, 153.025974])
 
     data_b = io.BytesIO()
     m.save(data_b, close_file=False)
 
-    w = QtWebEngineWidgets.QWebEngineView()
-    w.setHtml(data_b.getvalue().decode())
-    w.resize(640, 480)
-    w.show()
+    # Create GUI
+    app = QApplication(sys.argv)
+    window = QWidget()
+    # Right Frame (Map)
+    right_frame = QWidget()
+    right_frame_layout = QVBoxLayout()
+    map_view = QtWebEngineWidgets.QWebEngineView()
+    map_view.setHtml(data_b.getvalue().decode())
+    right_frame_layout.addWidget(map_view)
+    right_frame.setLayout(right_frame_layout)
 
-    update_map(w)
+    # Left Frame (Options)
+    left_frame = QWidget()
+    left_frame_layout = QVBoxLayout()
+    left_frame_layout.addWidget(QPushButton("Left 1"))
+    update_button = QPushButton("Update")
 
+    def update_map():
+        data_bytes = io.BytesIO()
+        new_map = folium.Map(location=[-25.290053, 152.864781])
+        new_map.save(data_bytes, close_file=False)
+        map_view.setHtml(data_bytes.getvalue().decode())
+
+    update_button.clicked.connect(update_map)
+    left_frame_layout.addWidget(update_button)
+    left_frame.setLayout(left_frame_layout)
+
+    # Window Layout
+    window_layout = QHBoxLayout()
+    window_layout.addWidget(left_frame)
+    window_layout.addWidget(right_frame)
+    window.setLayout(window_layout)
+
+    # Run App
+    window.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+
+    get_data()
+    create_gui()
 
